e6b81bdbb3170de29568d8eb258f9df01c1f09b0
priyanshu buley <buley2607@gmail.com>
2023-11-24 12:27:12 +0530

    adding user authentication component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import UserAuthentication from './component/UserAuthentication';
+
+function App() {
+  return (
+    <div className="App">
+      <UserAuthentication />
+    </div>
+  );
+}
+
+export default App;
